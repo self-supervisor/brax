e0a5955a9901164f68d9ddddfdf5418be53e2258
Augustine Mavor-Parker <augustine.mavor-parker@cs.ucl.ac.uk>
2023-09-22 16:49:45 +0100

    now works with wandb

--- notebooks/training.py
+++ notebooks/training.py
@@ -3,16 +3,15 @@ import os
 
 import jax
 
-os.environ["HYDRA_FULL_ERROR"] = "1"
-
 from datetime import datetime
 
 import flax
 import hydra
 import matplotlib.pyplot as plt
 from jax import numpy as jp
+import numpy as onp
 
-# import wandb
+import wandb
 from omegaconf import DictConfig
 
 import brax
@@ -20,64 +19,32 @@ from brax import envs
 from brax.io import html, json, model
 from brax.training.agents.ppo import train as ppo
 from brax.training.agents.sac import train as sac
+from typing import Dict
+
+
+def get_kwargs_ready(cfg: DictConfig) -> Dict:
+    kwargs = dict(cfg)
+    kwargs.pop("env_name")
+    kwargs.pop("backend")
+    kwargs.pop("training_algo")
+    kwargs.pop("max_y")
+    kwargs.pop("min_y")
+    return kwargs
 
 
 @hydra.main(config_path="conf")
 def main(cfg: DictConfig):
-    # wandb.init(project="sac_siren_brax", config=cfg)
+    wandb.init(project="sac_siren_brax", config=dict(cfg))
     env = envs.get_environment(env_name=cfg.env_name, backend=cfg.backend)
-    if cfg.env_name == "hopper" or cfg.env_name == "walker2d":
-        trainer = sac.train
-    elif (
-        cfg.env_name == "pusher"
-        or cfg.env_name == "reacher"
-        or cfg.env_name == "humanoidstandup"
-        or cfg.env_name == "humanoid"
-        or cfg.env_name == "halfcheetah"
-        or cfg.env_name == "ant"
-        or cfg.env_name == "inverted_double_pendulum"
-        or cfg.env_name == "inverted_pendulum"
-    ):
-        trainer = ppo.train
-    else:
-        raise ValueError("Invalid environment name")
-    train_fn = functools.partial(
-        trainer,
-        num_timesteps=cfg.num_timesteps,
-        num_evals=cfg.num_evals,
-        reward_scaling=cfg.reward_scaling,
-        episode_length=cfg.episode_length,
-        normalize_observations=cfg.normalize_observations,
-        action_repeat=cfg.action_repeat,
-        unroll_length=cfg.unroll_length,
-        num_minibatches=cfg.num_minibatches,
-        num_updates_per_batch=cfg.num_updates_per_batch,
-        discounting=cfg.discounting,
-        learning_rate=cfg.learning_rate,
-        entropy_cost=cfg.entropy_cost,
-        num_envs=cfg.num_envs,
-        batch_size=cfg.batch_size,
-        seed=cfg.seed,
-    )
-
-    xdata, ydata = [], []
-    times = [datetime.now()]
+    trainer = sac.train if cfg.training_algo == "sac" else ppo.train
+    kwargs = get_kwargs_ready(cfg)
+    train_fn = functools.partial(trainer, **kwargs)
 
     def progress(num_steps, metrics):
-        times.append(datetime.now())
-        xdata.append(num_steps)
-        ydata.append(metrics["eval/episode_reward"])
-        plt.xlim([0, train_fn.keywords["num_timesteps"]])
-        plt.ylim([cfg.min_y, cfg.max_y])
-        plt.xlabel("# environment steps")
-        plt.ylabel("reward per episode")
-        plt.plot(xdata, ydata)
-        plt.show()
+        metrics = {k: onp.array(v) for k, v in metrics.items()}
+        wandb.log(metrics, step=num_steps)
 
     make_inference_fn, params, _ = train_fn(environment=env, progress_fn=progress)
-
-    print(f"time to jit: {times[1] - times[0]}")
-    print(f"time to train: {times[-1] - times[1]}")
 
     model.save_params("/tmp/params", params)
     params = model.load_params("/tmp/params")
@@ -99,7 +66,7 @@ def main(cfg: DictConfig):
         state = jit_env_step(state, act)
 
     html_str = html.render(env.sys.replace(dt=env.dt), rollout)
-    # wandb.log({"trained_agent_html": wandb.Html(html_str)})
+    wandb.log({"trained_agent_html": wandb.Html(html_str)})
 
 
 if __name__ == "__main__":
